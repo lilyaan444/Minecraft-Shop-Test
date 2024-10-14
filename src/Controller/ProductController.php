d62e0e92480684aad9378a906df19e7ddcbe44cd
mullerlilyan <mullerlilyan@gmail.com>
2024-10-14 14:39:40 +0000

    Minecraft interface

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,6 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+
+
+
 
 #[Route('/product')]
 class ProductController extends AbstractController
@@ -32,7 +36,16 @@ class ProductController extends AbstractController
     {
         return $this->render('product/index.html.twig', [
             'products' => $productRepository->findAll(),
-            'categories' => $categoryRepository->findAll(),
         ]);
     }
+
+    #[Route('/{id}', name: 'app_product_show', methods: ['GET'])]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+
 }
